38ffd50b26e2fd9b01a6e6a7ee2ac9f29619e4cb
Jeniffer_caroline <jeniffercaroline530@gmail.com>
2025-03-13 19:14:01 -0300

    Criação das classes

--- src/Sustenta/Relatorio.java
+++
@@ -1,4 +0,0 @@
-package Sustenta;
-
-public class Relatorio {
-}
--- src/Sustenta/Voluntario.java
+++ src/Sustenta/Voluntario.java
@@ -1,4 +1,33 @@
 package Sustenta;
 
 public class Voluntario {
+    //Atributos
+    private String nome;
+    private String email;
+
+    public Voluntario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    //Métodos
+    public void participarProjeto(ProjetoSustentavel projeto){
+projeto.adicionarVoluntario(this);
+}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
